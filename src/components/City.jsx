9ef35bf6d673f5d55f5d697ed4eeca6c38de1144
Lucie Bučková <lucie.buckova@gmail.com>
2024-04-16 19:09:04 +0200

    Add all functionalities

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,9 @@
-import { useParams, useSearchParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import styles from './City.module.css';
+import { useCities } from '../contexts/CitiesContext';
+import Spinner from './Spinner';
+import ButtonBack from './ButtonBack';
+import { useEffect } from 'react';
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat('en', {
@@ -11,29 +15,20 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  const { getCity, currentCity, isLoading } = useCities();
 
-  // TEMP DATA
-  const currentCity = {
-    cityName: 'Lisbon',
-    emoji: '🇵🇹',
-    date: '2027-10-31T15:59:59.138Z',
-    notes: 'My favorite city so far!',
-  };
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id]
+  );
 
   const { cityName, emoji, date, notes } = currentCity;
 
+  if (isLoading) return <Spinner />;
+
   return (
-    <>
-      <h1>City {id}</h1>
-      <p>
-        Position: {lat}, {lng}
-      </p>
-    </>
-  );
-  /* return (
     <div className={styles.city}>
       <div className={styles.row}>
         <h6>City name</h6>
@@ -59,8 +54,7 @@ function City() {
         <a
           href={`https://en.wikipedia.org/wiki/${cityName}`}
           target="_blank"
-          rel="noreferrer"
-        >
+          rel="noreferrer">
           Check out {cityName} on Wikipedia &rarr;
         </a>
       </div>
@@ -69,7 +63,7 @@ function City() {
         <ButtonBack />
       </div>
     </div>
-  ); */
+  );
 }
 
 export default City;
